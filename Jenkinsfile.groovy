5ece6d91d32bca582bc65e89a3215c7e25abbb7b
seraaboagye <sera.aboagye@gmail.com>
2020-02-08 10:33:10 -0600

    packer pipeline

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,1 +1,14 @@
-
+node {
+    stage ("Pull Repo"){
+        echo "Hello"
+    }
+    stage ("Build Image"){
+        sh "packer version"
+    }
+    stage ("Send Notification to Slack"){
+        echo "Hello"
+    }
+    stage ("Send Email"){
+        echo "Hello"
+    }
+}
