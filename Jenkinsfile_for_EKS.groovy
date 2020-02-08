d9b2320cf59e8076c72c854a049d9586324abc1e
seraaboagye <sera.aboagye@gmail.com>
2020-02-08 12:52:33 -0600

    file added

---
+++ Jenkinsfile_for_EKS.groovy
@@ -0,0 +1,15 @@
+node{
+    stage("Pull Repo"){
+        git 'https://github.com/bkarakas/terraform-iaac-eks-burak'
+    }
+    stage("stage1"){
+        echo "Hello"
+    }
+    stage("stage1"){
+        echo "Hello"
+    }
+    stage("stage1"){
+        echo "Hello"
+
+    }
+}
