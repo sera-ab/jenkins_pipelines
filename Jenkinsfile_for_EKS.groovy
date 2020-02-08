64893b60f524cacb84ea64cb285f3ab80b91b0a5
seraaboagye <sera.aboagye@gmail.com>
2020-02-08 13:25:00 -0600

    EKS configured

--- Jenkinsfile_for_EKS.groovy
+++ Jenkinsfile_for_EKS.groovy
@@ -1,12 +1,21 @@
 node{
     stage("Pull Repo"){
-        git 'https://github.com/bkarakas/terraform-iaac-eks-burak'
+        ws {
+            git 'https://github.com/sera-ab/terraform_EKS.git'
+        }
+        
     }
-    stage("stage1"){
-        echo "Hello"
+    stage("Download Terraform"){
+        ws {
+            sh "terraform version"
+            sh "wget https://releases.hashicorp.com/terraform/0.12.19/terraform_0.12.19_linux_amd64.zip"
+            sh "unzip -o terraform_0.12.19_linux_amd64.zip"
+            sh "./terraform version"}
     }
-    stage("stage1"){
-        echo "Hello"
+    stage("Set Backend"){
+        ws {
+            sh "source setenv.sh configurations/dev/us-west-2/dev.tfvars"
+        }
     }
     stage("stage1"){
         echo "Hello"
