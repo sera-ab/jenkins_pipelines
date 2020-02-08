f656627218a4fc10e20c896c0a3ebe68f609f0f8
seraaboagye <sera.aboagye@gmail.com>
2020-02-08 11:24:57 -0600

    my

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,3 +1,4 @@
+  
 node {
     properties([parameters([choice(choices: ['us-east-1', 'us-east-2', 'us-west-1', 'us-west-2'], description: 'Please select a region', name: 'AMI_REGION')])])
 
@@ -6,14 +7,12 @@ node {
     }
     stage("Build Image"){
         sh "packer version"
-        sh "packer  build -var region=${AMI_REGION} tools/jenkins_example.json"
+        //sh "packer  build -var region=${AMI_REGION} tools/jenkins_example.json"
     }
     stage("Send Notification to Slack"){
         slackSend channel: 'nagios_alerts', message: 'Golden AMI has been built'
     }
     stage("Send Email"){
-        mail bcc: '', body: '''Hello, 
-            Your AMI is ready in us-east-1
-            Thanks''', cc: '', from: '', replyTo: '', subject: 'Golden AMI has been built', to: 'seraaboagye.it@gmail.com'
+        mail bcc: '', body: '''Hello, Your AMI is ready in us-east-1 Thanks''', cc: '', from: '', replyTo: '', subject: 'Golden AMI has been built', to: 'farrukh@gmail.com'
     }
 }
